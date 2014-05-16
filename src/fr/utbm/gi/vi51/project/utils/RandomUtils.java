ef533054661a2f8146742ddf2cf4fbd52dfe40ba
Flikalb <lucie.boutou@gmail.com>
2014-05-16 19:43:05 +0200

    pouet

---
+++ src/fr/utbm/gi/vi51/project/utils/RandomUtils.java
@@ -0,0 +1,22 @@
+package fr.utbm.gi.vi51.project.utils;
+
+import java.util.Random;
+
+public final class RandomUtils {
+
+	public static float randomNumber() {
+	    Random generator = new Random();
+	    float num = generator.nextFloat();
+
+	    return num;
+	}
+	
+	public static float randomNumber(float minimum, float maximum) {
+		return minimum + randomNumber() * maximum;
+	}
+	
+	public static float randomBinomial(float value) {
+		return randomNumber(0,value)-randomNumber(0,value);
+	}
+	
+}
